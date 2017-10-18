2864a891d6881b505f6772c3054f09ce8ea4b3a0
Kazi Rahamatullah <kazirahmat58@gmail.com>
2017-10-18 15:51:46 +0600

    CRUD With OOP and MYSQLi

---
+++ inc/footer.php
@@ -0,0 +1,12 @@
+</section> 
+
+<section class="footeroption">
+			  
+		<p>&copy;<?php echo date("Y");?> Training with Live project</p>	     
+		<h2><?php echo"www.raHMat project.com";?></h2>
+	</section>  
+</div>
+
+</body>	
+
+</html>
---
+++ inc/header.php
@@ -0,0 +1,34 @@
+<?php
+	$fonts="verdana";
+
+?>
+
+<!DOCTYPE html>
+<html>
+   <head>
+	   <title>PHP Syntax</title>
+		<style>
+		   body{font-family:<?php echo $fonts;?>}
+		  .phpcoding{width:900px;margin:0 auto;background:#ddd;}
+           .headeroption,.footeroption{background:#333;color:#fff;text-align:center;padding: 20px;}
+           .headeroption h2,.footeroption h2{margin:0;}		  
+           .maincontent{min-height:500px;padding: 20px;}
+            p{margin:0;}
+            input[type="text"]{width:250px;}
+			 select{font-size:18px;padding:2px 5px;width:250px}
+			 .tblone{width:100%;border:1px solid #fff;margin:20px 0}
+			 .tblone td{padding:5px 10px;text-align:center}
+			 table.tblone tr:nth-child(2n+1){background:#fff; height:30px}
+			 table.tblone tr:nth-child(2n){background:#f1f1f1; height:30px}
+		</style>
+	</head>
+   <body>
+	  <div class="phpcoding">
+			  <section class="headeroption">
+				  <h2>CRUD Using OOP PHP and MYSQLi</h2>
+			  </section>
+			  
+			  
+			  <section class="maincontent">
+			   
+				
